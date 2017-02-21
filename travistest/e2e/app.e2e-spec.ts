ff45106961f5babac63e0cbde07d9874d6e2d5ba
Fabian Grimminger <grimmingerapps@gmail.com>
2017-02-21 22:39:30 +0100

    new project

---
+++ travistest/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TravistestPage } from './app.po';
+
+describe('travistest App', () => {
+  let page: TravistestPage;
+
+  beforeEach(() => {
+    page = new TravistestPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
