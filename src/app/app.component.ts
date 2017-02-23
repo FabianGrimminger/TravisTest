d47a9e454ee18e9868861cb142a75962fb7cdbf7
Fabian Grimminger <grimmingerapps@gmail.com>
2017-02-23 11:18:54 +0100

    test lint fail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works fine yeah!';
+  title = 
+  
+  
+  'app works fine yeah!';
 }
